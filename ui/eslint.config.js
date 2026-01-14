bcda8e85b512c3cb14c0a4ba4adfbc3faf33630a
Irving Dinh <irving.dinh@gmail.com>
2026-01-14 21:33:57 +0700

    feat(ui): Added shadcn/ui

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -1,16 +1,16 @@
-import js from '@eslint/js'
-import globals from 'globals'
+import js from "@eslint/js";
+import globals from "globals";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -18,16 +18,17 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     plugins: {
-      'simple-import-sort': simpleImportSort,
+      "simple-import-sort": simpleImportSort,
     },
     rules: {
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error',
+      "react-refresh/only-export-components": "off",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-  eslintConfigPrettier
-])
+  eslintConfigPrettier,
+]);
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button.tsx";
+import { ThemeProvider } from "@/components/ui/theme-provider.tsx";
+
 function App() {
   return (
-    <p className="p-4">
-      <p>Lorem ipsum dolor sit amet</p>
-    </p>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </ThemeProvider>
   );
 }
 
