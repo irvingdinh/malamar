6f312192bb56157ade5e692a84d5b5286341cda5
Irving Dinh <irving.dinh@gmail.com>
2026-01-14 21:24:39 +0700

    chore(ui): Updated eslint.config.js

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -16,6 +17,13 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
